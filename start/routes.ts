19ca3586954d9468231bf3801c8c657a5e66132b
RichardKoudou <richard.koudou@epsi.fr>
2025-05-13 20:16:51 +0200

    add : docker compose + mise en commentaire du code du swagger

--- start/routes.ts
+++ start/routes.ts
@@ -11,19 +11,19 @@ import { middleware } from './kernel.js'
 const UsersController = () => import('#controllers/users_controller')
 const AnimalController = () => import('#controllers/animals_controller')
 
-import AutoSwagger from "adonis-autoswagger";
-import swagger from "#config/swagger";
+//import AutoSwagger from "adonis-autoswagger";
+//import swagger from "#config/swagger";
 // returns swagger in YAML
-router.get("/swagger", async () => {
-  return AutoSwagger.default.docs(router.toJSON(), swagger);
-});
+//router.get("/swagger", async () => {
+  //return AutoSwagger.default.docs(router.toJSON(), swagger);
+//});
 
 // Renders Swagger-UI and passes YAML-output of /swagger
-router.get("/docs", async () => {
-  return AutoSwagger.default.ui("/swagger", swagger);
+//router.get("/docs", async () => {
+  //return AutoSwagger.default.ui("/swagger", swagger);
   // return AutoSwagger.default.scalar("/swagger"); to use Scalar instead
   // return AutoSwagger.default.rapidoc("/swagger", "view"); to use RapiDoc instead (pass "view" default, or "read" to change the render-style)
-});
+//});
 
 router
   .get('OPoil/v1/Home', async () => {})
